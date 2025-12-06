744e374e770a8476e66b1e27ca4100541154605e
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-12-06 11:13:59 +0900

    add: global gitignoreを追加

--- home/programs/files.nix
+++ home/programs/files.nix
@@ -3,5 +3,6 @@
     ".bashrc".source = ../../dotfiles/bashrc;
     ".bash_profile".source = ../../dotfiles/bash_profile;
     ".config/zed/settings.json".source = ../../dotfiles/zed_settings.json;
+    ".config/git/ignore".source = ../../dotfiles/gitignore;
   };
 }
