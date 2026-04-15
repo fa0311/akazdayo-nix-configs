eee0e29f9913f5f89b4c2ba2c76bf133118a34ef
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-04-15 16:08:45 +0900

    smear-cursorを入れた

--- home/programs/nixvim/plugins/ui/default.nix
+++ home/programs/nixvim/plugins/ui/default.nix
@@ -4,6 +4,7 @@
     ./lualine.nix
     ./neo-tree.nix
     ./indent-blankline.nix
+    ./smear-cursor.nix
     ./which-key.nix
     ./web-devicons.nix
   ];
---
+++ home/programs/nixvim/plugins/ui/smear-cursor.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+  programs.nixvim.plugins.smear-cursor = {
+    enable = true;
+    autoLoad = true;
+    settings = { };
+  };
+}
