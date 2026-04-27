10c48eb1eef988f4f8952c73fe8a30d70afd04ce
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-04-27 22:48:46 +0900

    lixのcache設定をした

--- hosts/darwin-common/default.nix
+++ hosts/darwin-common/default.nix
@@ -4,6 +4,13 @@
     "nix-command"
     "flakes"
   ];
+  nix.settings.always-allow-substitutes = true;
+  nix.settings.extra-trusted-substituters = [
+    "https://cache.lix.systems"
+  ];
+  nix.settings.extra-trusted-public-keys = [
+    "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+  ];
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = hostMeta.system;
