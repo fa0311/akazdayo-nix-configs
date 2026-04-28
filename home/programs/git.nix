7aefe2198369affc5186926653c936fbc4b301c2
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-04-28 15:48:35 +0900

    コミット署名するようにしたはず

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -11,14 +11,26 @@
         user = {
           name = "akazdayo";
           email = "82073147+akazdayo@users.noreply.github.com";
+        }
+        // lib.optionalAttrs pkgs.stdenv.isDarwin {
+          signingKey = "~/.ssh/id_ed25519_sk_rk.pub";
         };
         init = {
           defaultBranch = "main";
         };
       }
       // lib.optionalAttrs pkgs.stdenv.isDarwin {
+        commit = {
+          gpgsign = true;
+        };
         core = {
           sshCommand = "/opt/homebrew/bin/ssh";
+        };
+        gpg = {
+          format = "ssh";
+          ssh = {
+            program = "/opt/homebrew/bin/ssh-keygen";
+          };
         };
       };
   };
