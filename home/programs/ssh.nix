2b7774c60df08ea132ba669489ddbd74b81f59e8
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-05-05 16:54:51 +0900

    sk鍵で動かなかったのを直してみた

--- home/programs/ssh.nix
+++ home/programs/ssh.nix
@@ -5,16 +5,19 @@
       "192.168.11.50" = {
         user = "akazdayo";
         identityFile = "~/.ssh/id_ed25519_sk_rk";
+        identityAgent = "none";
       };
       "github.com" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519_sk_rk";
+        identityAgent = "none";
         identitiesOnly = true;
       };
     } // lib.optionalAttrs pkgs.stdenv.isDarwin {
       "*" = lib.hm.dag.entryAfter [ "github.com" ] {
         identityFile = "~/.ssh/id_ed25519_sk_rk";
+        identityAgent = "none";
       };
     };
   };
