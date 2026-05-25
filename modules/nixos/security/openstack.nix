03adc9a0ccf2355f82321de620043a8ae6441677
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-05-26 02:40:57 +0900

    hotfix: fail2banミスってた

--- modules/nixos/security/openstack.nix
+++ modules/nixos/security/openstack.nix
@@ -2,23 +2,17 @@
 {
   services.fail2ban = {
     enable = true;
-    daemonConfig = ''
-      [DEFAULT]
-      findtime = 10m
-      bantime = 1h
-      maxretry = 5
-      backend = systemd
-    '';
     jails = {
-      sshd = ''
-        enabled = true
-        port = ssh
-        filter = sshd
-        logpath = /var/log/auth.log
-        maxretry = 5
-        findtime = 600
-        bantime = 3600
-      '';
+      sshd = {
+        settings = {
+          port = "ssh";
+          filter = "sshd";
+          logpath = "/var/log/auth.log";
+          maxretry = 5;
+          findtime = 600;
+          bantime = 3600;
+        };
+      };
     };
   };
 }
